65332567749340628e3dfe42a1c0c856869d2164
Amar Mallick <amarmallick.sqa@gmail.com>
2026-04-11 07:56:41 +0530

    commit: Switch case example....

---
+++ Java_Programming/src/test/java/practice_code/SwitchCase_Practice.java
@@ -0,0 +1,29 @@
+package practice_code;
+
+public class SwitchCase_Practice {
+
+	public static void main(String[] args) {
+		// Try to find a Month by using Switch case............
+		int month = 3;
+		switch (month) {
+		case 1:
+			System.out.println("January");
+			break;
+		case 2:
+			System.out.println("February");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+		default:
+			System.out.println("Month is not exist ...");
+		}
+
+	}
+
+}
